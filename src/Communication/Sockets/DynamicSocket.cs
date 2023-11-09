92bbeea3576dc223c0ddd888e57e968fae7e22f2
Jan Zenisek <janzenisek@gmx.at>
2023-11-09 20:07:14 +0100

    added stubs for shared memory and dynamic socket

---
+++ src/Communication/Sockets/DynamicSocket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAT.Communication.Sockets {
+  public class DynamicSocket {
+
+    // TBD: hosts different sockets and dynamically at runtime (optionally also autonomously) decides which to use
+    // e.g. select between network-based socket (e.g. mqtt) and shared memory based socket at runtime
+
+  }
+}
---
+++ src/Communication/Sockets/SharedMemorySocket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAT.Communication.Sockets {
+  public class SharedMemorySocket {
+
+    // TBD: will support request/response communication model via non-persisted memory mapped files, i.e. shared memory
+
+  }
+}
